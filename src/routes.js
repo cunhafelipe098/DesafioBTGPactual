eaebaf05c5e9bfe6fa5446e1a850d9e5d73a202b
Felipe Vieira <cunhafelipe098@gmail.com>
2020-08-12 23:37:52 -0300

    feat: create users model

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
 
+import User from './app/models/User'
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'hello world' })
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'felipe',
+    cpf: 'defdrf',
+    rg: 'defcrv',
+    password_hash: '555655' 
+  })
+
+  return res.json(user);
 })
 
 export default routes;
