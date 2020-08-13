31d95c5e3b828f84c6b64e7fb31637ade019199a
Felipe Vieira <cunhafelipe098@gmail.com>
2020-08-12 23:39:52 -0300

    feat: create users controllers

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,11 +1,17 @@
+ 
+import * as Yup from 'yup'
 import User from '../models/User'
 
 class UserController {
   async store (req, res) {
     
     const user = await User.create(req.body)
-
-    return res.json(user)
+    return res.json({
+      id,
+      name,
+      email,
+      provider
+    })
   }
 }
 
