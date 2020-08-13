d3187e1bd76dde9413c890c65723cce0ad7f1e66
Felipe Vieira <cunhafelipe098@gmail.com>
2020-08-12 23:56:45 -0300

    feat: user sing-up implementation with validation of fields

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,13 +4,34 @@ import User from '../models/User'
 
 class UserController {
   async store (req, res) {
-    
-    const user = await User.create(req.body)
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      cpf: Yup.string().required().min(11),
+      rg: Yup.string().required().min(9),
+      password: Yup.string().required().min(6)
+    })
+
+    if (!await schema.isValid(req.body)) {
+      return res.status(400).json({ error: 'Validation fails' })
+    }
+
+    const cpfExists = await User.findOne({ where: { cpf: req.body.cpf } })
+
+    if (cpfExists) {
+      return res.status(400).json({ error: 'CPF already exists.' })
+    }
+
+    const rgExists = await User.findOne({ where: { rg: req.body.rg } })
+
+    if (rgExists) {
+      return res.status(400).json({ error: 'RG already exists.' })
+    }
+
+    const { id, name } = await User.create(req.body)
+
     return res.json({
       id,
       name,
-      email,
-      provider
     })
   }
 }
